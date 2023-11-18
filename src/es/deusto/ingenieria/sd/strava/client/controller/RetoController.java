50f0cfad9bce66592521493dde11bd924e928c78
JaimeRevilla <jaimerevilla@opendeusto.es>
2023-11-18 12:33:45 +0100

    a
    Co-Authored-By: Enekourquijo <90685976+Enekourquijo@users.noreply.github.com>
    Co-Authored-By: Markeliturbe <92159698+Markeliturbe@users.noreply.github.com>
    Co-Authored-By: Pau <114235186+PauMartin8@users.noreply.github.com>

--- src/es/deusto/ingenieria/sd/strava/client/controller/RetoController.java
+++ src/es/deusto/ingenieria/sd/strava/client/controller/RetoController.java
@@ -10,48 +10,48 @@ public class RetoController {
 	public RetoController(ServiceLocator serviceLocator) {
 		this.serviceLocator = serviceLocator;
 	}
-	public List<String> getDeporte() {
-        try {
-            return this.serviceLocator.getService().getDeporteRet();
-        } catch (Exception e) {
-            System.out.println(" # Error during getDepRet: " + e);
-            return null;
-        }
-    }
-
-    public void makeReto(String nombre, String fIni, String fFin, double dist, double obj, String deporte) {
-        try {
-            this.serviceLocator.getService().makeReto(nombre, fIni, fFin, dist, obj, deporte);
-        } catch(Exception e) {
-            System.out.println(" # Error during reto making: " + e);
-        }
-    }
-
-    public List<String> getReto() {
-        try {
-            List<String> list = this.serviceLocator.getService().getReto();
-            return list;
-        } catch(Exception e) {
-            System.out.println(" # Error during get Reto: " + e);
-            return null;
-        }
-    }
-
-    public List<String> getRetoAct() {
-        try {
-            return this.serviceLocator.getService().getRetoActivado();
-        } catch(Exception e) {
-            System.out.println(" # Error during get RetoAct: " + e);
-            return null;
-        }
-    }
-
-    public void makeRetoAct(String s) {
-        try {
-            this.serviceLocator.getService().activateReto(s);
-        } catch(Exception e) {
-            System.out.println(" # Error Activate Reto: " + e);
-        }
-    }
+//	public List<String> getDeporte() {
+//        try {
+//            return this.serviceLocator.getService().getDeporteRet();
+//        } catch (Exception e) {
+//            System.out.println(" # Error during getDepRet: " + e);
+//            return null;
+//        }
+//    }
+//
+//    public void makeReto(String nombre, String fIni, String fFin, double dist, double obj, String deporte) {
+//        try {
+//            this.serviceLocator.getService().makeReto(nombre, fIni, fFin, dist, obj, deporte);
+//        } catch(Exception e) {
+//            System.out.println(" # Error during reto making: " + e);
+//        }
+//    }
+//
+//    public List<String> getReto() {
+//        try {
+//            List<String> list = this.serviceLocator.getService().getReto();
+//            return list;
+//        } catch(Exception e) {
+//            System.out.println(" # Error during get Reto: " + e);
+//            return null;
+//        }
+//    }
+//
+//    public List<String> getRetoAct() {
+//        try {
+//            return this.serviceLocator.getService().getRetoActivado();
+//        } catch(Exception e) {
+//            System.out.println(" # Error during get RetoAct: " + e);
+//            return null;
+//        }
+//    }
+//
+//    public void makeRetoAct(String s) {
+//        try {
+//            this.serviceLocator.getService().activateReto(s);
+//        } catch(Exception e) {
+//            System.out.println(" # Error Activate Reto: " + e);
+//        }
+//    }
 
 }
